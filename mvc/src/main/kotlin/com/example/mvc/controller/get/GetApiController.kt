d26d0eecb104add6c91f34bd8d03a565133c158b
greeng00se <thegreengoose7@gmail.com>
2022-08-04 20:01:23 +0900

    GetMapping

---
+++ mvc/src/main/kotlin/com/example/mvc/controller/get/GetApiController.kt
@@ -0,0 +1,50 @@
+package com.example.mvc.controller.get
+
+import com.example.mvc.controller.model.http.UserRequest
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api")
+class GetApiController {
+
+    @GetMapping(path = ["/hello", "/hello2"])
+    fun hello(): String {
+        return "HELLO WORLD"
+    }
+
+    @RequestMapping(method = [RequestMethod.GET], path = ["/request-mapping"])
+    fun requestMapping(): String {
+        return "request mapping"
+    }
+
+    @GetMapping("/get-mapping/path-variable/{name}/{age}")
+    fun pathVariable(@PathVariable name: String, @PathVariable age: String): String {
+        return "$name $age";
+    }
+
+    @GetMapping("/get-mapping/path-variable2/{name}/{age}")
+    fun pathVariable2(@PathVariable(value = "name") _name: String, @PathVariable(name = "age") _age: String): String {
+        val name = "kotlin"
+        return "$name $_age";
+    }
+
+    @GetMapping("/get-mapping/query-param")
+    fun queryParam(
+        @RequestParam(name = "name") name: String,
+        @RequestParam(value = "age") age: Int
+    ): String {
+        return "$name $age"
+    }
+
+    @GetMapping("/get-mapping/query-param/object")
+    fun queryParamObject(userRequest: UserRequest): UserRequest {
+        return userRequest
+    }
+    @GetMapping("/get-mapping/query-param/map")
+    fun queryParamMap(@RequestParam map: Map<String, Any>): Map<String, Any> {
+        val phoneNumber = map.get("phone-number")
+        println(phoneNumber)
+        return map
+    }
+
+}
---
+++ mvc/src/main/kotlin/com/example/mvc/controller/model/http/UserRequest.kt
@@ -0,0 +1,9 @@
+package com.example.mvc.controller.model.http
+
+data class UserRequest(
+    var name:String?=null,
+    var age:Int?=null,
+    var email:String?=null,
+    var address: String?=null,
+    var phoneNumber: String?=null
+)
